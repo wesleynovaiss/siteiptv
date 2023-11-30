6eadaade0a6702b557f47f7b4371312ead14508a
Wesley Novais <125233293+wesleynovaiss@users.noreply.github.com>
2023-11-30 11:47:17 +0100

    siteiptv

---
+++ contato.py
@@ -0,0 +1,4 @@
+contato = int(input('Qual é o seu numero?'))
+print('O numero é {}'.format(contato))
+email = (input('Qual é seu email?'))
+print('Seu email é {}'.format(email))
---
+++ site.py
@@ -0,0 +1,2 @@
+name = str(input('Qual e seu nome?'))
+print('seu nome é {}'.format(name))
